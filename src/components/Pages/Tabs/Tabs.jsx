0e615c68cace2116bad0c779a7c1e2d5a730193f
AntonChetveryk <chetverykanton92@gmail.com>
2020-02-07 09:55:19 +0200

    last

--- src/components/Pages/Tabs/Tabs.jsx
+++ src/components/Pages/Tabs/Tabs.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route, Switch } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import classnames from "classnames";
 import Detail from "../Tabs/Detail";
 import Videos from "../Tabs/Videos";
@@ -25,72 +26,74 @@ const Tabs = props => {
 
   return (
     <BrowserRouter>
-      <div>
-        <Nav tabs className="mt-4">
-          <NavItem>
-            <NavLink
-              className={classnames({ active: activeTab === "1" })}
-              onClick={() => {
-                toggle("1");
-              }}
-            >
-              Детали
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              className={classnames({ active: activeTab === "2" })}
-              onClick={() => {
-                toggle("2");
-              }}
-            >
-              Похожие фильмы
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              to={`/movie/${item.id}/actors`}
-              className={classnames({ active: activeTab === "3" })}
-              onClick={() => {
-                toggle("3");
-              }}
-            >
-              Актеры
-            </NavLink>
-          </NavItem>
-        </Nav>
-        <TabContent activeTab={activeTab}>
-          <TabPane tabId="1">
-            <Row>
-              <Col sm="12">
-                <Route exact path={`/movie/${item.id}`} component={Detail} />
-              </Col>
-            </Row>
-          </TabPane>
-          <TabPane tabId="2">
-            <Row>
-              <Col sm="12">
-                <Route
-                  exact
-                  path={`/movie/${item.id}/videos`}
-                  component={Videos}
-                />
-              </Col>
-            </Row>
-          </TabPane>
-          <TabPane tabId="3">
-            <Row>
-              <Col sm="12">
-                <Route
-                  exact
-                  path={`/movie/${item.id}/credits`}
-                  component={Credits}
-                />
-              </Col>
-            </Row>
-          </TabPane>
-        </TabContent>
-      </div>
+      <Switch>
+        <div>
+          <Nav tabs className="mt-4">
+            <NavItem>
+              <NavLink
+                className={classnames({ active: activeTab === "1" })}
+                onClick={() => {
+                  toggle("1");
+                }}
+                to={`/movie/${item.id}/`}
+              >
+                Детали
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                to={`/movie/${item.id}/videos`}
+                className={classnames({ active: activeTab === "2" })}
+                onClick={() => {
+                  toggle("2");
+                }}
+              >
+                Похожие фильмы
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                to={`/movie/${item.id}/credits`}
+                className={classnames({ active: activeTab === "3" })}
+                onClick={() => {
+                  toggle("3");
+                }}
+              >
+                Актеры
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <TabContent activeTab={activeTab}>
+            <TabPane tabId="1">
+              <Row>
+                <Col sm="12">
+                  <Route exact path={`/movie/${item.id}`}>
+                    <Detail />
+                  </Route>
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane tabId="2">
+              <Row>
+                <Col sm="12">
+                  <Route exact path={`/movie/${item.id}/videos`}>
+                    <Videos />
+                  </Route>
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane tabId="3">
+              <Row>
+                <Col sm="12">
+                  <Route exact path={`/movie/${item.id}/credits`}>
+                    <Credits />
+                  </Route>
+                </Col>
+              </Row>
+            </TabPane>
+          </TabContent>
+        </div>
+      </Switch>
     </BrowserRouter>
   );
 };
