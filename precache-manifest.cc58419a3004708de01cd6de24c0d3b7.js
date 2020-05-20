da34bc53e3c5db6d637185cf7824d9b5a99c3860
AntonChetveryk <chetverykanton92@gmail.com>
2020-05-20 20:33:44 +0300

    Updates

---
+++ precache-manifest.cc58419a3004708de01cd6de24c0d3b7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd690c23b51a622673522c60b2c0db56",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "d6bba7590e5038594d43",
+    "url": "/movie_app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "5bc99f28c9f7b20e922b",
+    "url": "/movie_app/static/css/main.b69caf7c.chunk.css"
+  },
+  {
+    "revision": "d6bba7590e5038594d43",
+    "url": "/movie_app/static/js/2.8e41a60b.chunk.js"
+  },
+  {
+    "revision": "5bc99f28c9f7b20e922b",
+    "url": "/movie_app/static/js/main.58dbf3ab.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  },
+  {
+    "revision": "84162454817b67e1135b240991a8b333",
+    "url": "/movie_app/static/media/bookmark-24px.84162454.svg"
+  },
+  {
+    "revision": "3f1c7930747e9182a578003c96eb4838",
+    "url": "/movie_app/static/media/bookmark_border-24px.3f1c7930.svg"
+  },
+  {
+    "revision": "f894bce27d96ece589187a465b9dd588",
+    "url": "/movie_app/static/media/spinner.f894bce2.gif"
+  },
+  {
+    "revision": "d036747814b227e57059b656104e11af",
+    "url": "/movie_app/static/media/star-24px.d0367478.svg"
+  },
+  {
+    "revision": "f02a9cc48ed93dd5af0de8b96bbae622",
+    "url": "/movie_app/static/media/star_border-24px.f02a9cc4.svg"
+  }
+]);
