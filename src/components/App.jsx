5b73101222fc6e39781bb9ebc01eb94860d7d2bc
AntonChetveryk <chetverykanton92@gmail.com>
2020-06-23 18:59:23 +0300

    basename

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,32 @@
-import React from "react";
+import React from 'react'
 
-import Header from "./Header/Header";
-import MoviesPage from "./Pages/MoviesPage/MoviesPage";
-import MoviePage from "./Pages/MoviePage/MoviePage";
-import { BrowserRouter, Route } from "react-router-dom";
-import { withAuth } from "../hoc/withAuth";
+import Header from './Header/Header'
+import MoviesPage from './Pages/MoviesPage/MoviesPage'
+import MoviePage from './Pages/MoviePage/MoviePage'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { withAuth } from '../hoc/withAuth'
 
 class App extends React.Component {
   componentDidMount() {
-    const { auth, authActions } = this.props;
+    const { auth, authActions } = this.props
 
     if (auth.session_id) {
-      authActions.fetchAuth(auth.session_id);
+      authActions.fetchAuth(auth.session_id)
     }
   }
 
   render() {
-    const { auth } = this.props;
+    const { auth } = this.props
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="movie_app">
         <div>
           <Header user={auth.user} />
           <Route exact path="/" component={MoviesPage} />
           <Route path="/movie/:id" component={MoviePage} />
         </div>
       </BrowserRouter>
-    );
+    )
   }
 }
 
-export default withAuth(App);
+export default withAuth(App)
