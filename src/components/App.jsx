a63fc8d33cfcd9080e531d067362f4ffb1d7bb59
AntonChetveryk <chetverykanton92@gmail.com>
2020-12-04 11:47:22 +0200

    add hashrouter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import Layout from './Layout'
 // import Header from './Header/Header'
 import MoviesPage from './Pages/MoviesPage/MoviesPage'
 import MoviePage from './Pages/MoviePage/MoviePage'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 import { withAuth } from '../hoc/withAuth'
 
 class App extends React.Component {
@@ -18,14 +18,14 @@ class App extends React.Component {
   render() {
     const { auth } = this.props
     return (
-      <Router basename="movie_app">
+      <HashRouter>
         <Switch>
           <Layout auth={auth}>
             <Route exact path="/" component={MoviesPage} />
             <Route path="/movie/:id" component={MoviePage} />
           </Layout>
         </Switch>
-      </Router>
+      </HashRouter>
     )
   }
 }
